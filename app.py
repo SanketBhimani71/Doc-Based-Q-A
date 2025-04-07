f31720fe8567097e5b334c9c3d84ed31c3b3a9f6
Sanket Bhimani <86454766+SanketBhimani71@users.noreply.github.com>
2025-04-07 15:03:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import sys
 import pysqlite3
 sys.modules['sqlite3'] = sys.modules['pysqlite3']
 
-from langchain_unstructured import UnstructuredLoader as UnstructuredFileLoader
+from langchain.document_loaders import UnstructuredFileLoader
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
