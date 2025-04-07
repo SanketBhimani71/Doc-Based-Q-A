5454d11c6713ac5648606395621d5183d2604edb
Sanket Bhimani <86454766+SanketBhimani71@users.noreply.github.com>
2025-04-07 13:46:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,3 @@
-import sys
-import pysqlite3
-sys.modules["sqlite3"] = pysqlite3
-
-
 from langchain_unstructured import UnstructuredLoader
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -18,7 +13,9 @@ import tempfile
 import shutil
 import os
 from langchain_community.vectorstores.utils import filter_complex_metadata
+import chromadb.api
 
+chromadb.api.client.SharedSystemClient.clear_system_cache()
 
 
 st.title('Q&A Chatbot')
@@ -27,35 +24,50 @@ uploaded_file = st.sidebar.file_uploader(
     "Upload your document (PDF, Markdown, TXT, DOCX)", type=["pdf", "md", "txt", "docx"]
 )
 
+
 def file_upload():
+
+    # Create a temporary directory to store uploaded file
     temp_dir = tempfile.mkdtemp()
     temp_file_path = os.path.join(temp_dir, uploaded_file.name)
+
     with open(temp_file_path, "wb") as f:
         f.write(uploaded_file.read())
+
     st.sidebar.success("File uploaded successfully!")
+
     loader = UnstructuredLoader(temp_file_path)
     pages = loader.load()
     st.sidebar.success("Document loaded successfully!")
     return pages
 
+
 def process_file():
     if uploaded_file:
+
         data = file_upload()
         return data
+
+
+
 
 data = process_file()
 load_dotenv()
 
+
 def embedding_store(data):
+
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000)
     docs = text_splitter.split_documents(data)
+
     print("Total number of documents: ", len(docs))
 
+
     vectorstore = Chroma.from_documents(
-        documents=filter_complex_metadata(docs),
-        embedding=GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
-    )
+        documents=filter_complex_metadata(docs), embedding=GoogleGenerativeAIEmbeddings(model="models/text-embedding-004"))
+
     return vectorstore
+
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-1.5-pro",
@@ -66,29 +78,36 @@ llm = ChatGoogleGenerativeAI(
 )
 
 system_prompt = """
-  You are an assistant for questions-answering tasks.
-  Use the following pieces of retrieved context to answer
-  the question. If you don't know the answer, say that you
-  don't know. Use three sentences maximum and keep the
-  answer concise.
-
-  {context}
+  "You are an assistant for questions-answering tasks. "
+  "Use the following pieces of retrieved context to answer"
+  "the question. If you don't know the answer, say that you"
+  "don't know. Use three sentences maximum and keep the"
+  "answer concise"
+  "\n\n"
+  "{context}"
 """
 
 prompt = ChatPromptTemplate.from_messages([
     ("system", system_prompt),
     ("human", "{input}"),
 ])
-
 if data:
+    
     vectorstore = embedding_store(data)
-    retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 10})
+    
+    retriever = vectorstore.as_retriever(
+        search_type="similarity", search_kwargs={"k": 10})
 
     query = st.chat_input('Say something')
     input_query = query
 
     if input_query:
-        question_answer_chain = create_stuff_documents_chain(llm, prompt)
-        rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+
+        question_answer_chain = create_stuff_documents_chain(
+            llm, prompt)
+        rag_chain = create_retrieval_chain(
+            retriever, question_answer_chain)
+
         response = rag_chain.invoke({"input": input_query})
+
         st.write(response["answer"])
